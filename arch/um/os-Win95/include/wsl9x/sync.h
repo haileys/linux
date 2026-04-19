e3d337a60b7334afc7f7196660841b1cdea487a2
Hailey Somerville <hailey@hails.org>
2026-04-19 21:49:05 +1000

    add missing flags to VMM_BlockOnID

--- arch/um/os-Win95/include/wsl9x/sync.h
+++ arch/um/os-Win95/include/wsl9x/sync.h
@@ -11,4 +11,6 @@ void VMM_EnterMutex(HMUTEX mutex, unsigned long flags);
 void VMM_LeaveMutex(HMUTEX mutex);
 
 void VMM_SignalID(uint32_t id);
-void VMM_BlockOnID(uint32_t id);
+void VMM_BlockOnID(uint32_t id, unsigned long flags);
+#define BLOCK_SVC_INTS    (1 << 0)
+#define BLOCK_THREAD_IDLE (1 << 4)
